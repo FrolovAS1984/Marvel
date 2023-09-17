d7dc508a8f3881f5301adb8dd51016193c76a79c
Frolov_AS <slaider84@list.ru>
2023-09-17 16:33:45 +0300

    17.09.2023

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,7 +10,7 @@ module.exports = {
                 "node": true
             },
             "files": [
-                ".eslintrc.{js,cjs, css}"
+                ".eslintrc.{js,cjs}"
             ],
             "parserOptions": {
                 "sourceType": "script"
